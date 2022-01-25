fea875855dd345ac1d075c976a85058b0c0fbe80
phyop <seek1andfind2@gmail.com>
2022-01-25 21:27:05 +0800

    commit

--- 220125_what_day_is_today.py
+++ 220125_what_day_is_today.py
@@ -1,17 +1,15 @@
-def what_day():
+def what_day(year, month, day, yuandan):
     pass
 
 if __name__ == "__main__":
-
     path = "./in.txt"
-
     f = open(path,"r")
-    lines = f.readlines()
-
-    year = lines[0].split()[0]
-    yuandan = lines[0].split()[1]
-    count = lines[1]
-
-
-
-    f.close()
+    line_1 = f.readline()
+    year, yuandan = line_1[0].split()[0], line_1[0].split()[1]
+    count = f.readline()
+    for i in range(count):
+        line = f.readline()
+        month, day = line.split()[0], line[0].split()[1]
+        # what_day(year, month, day, yuandan)
+        print("year, month, day, yuandan = ", year, month, day, yuandan)
+    f.close()    
