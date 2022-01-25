2533e7b88e97eb2fe9d9e0385c56b3ccbacd676b
phyop <seek1andfind2@gmail.com>
2022-01-25 22:10:22 +0800

    def open_today(path):

--- 220125_what_day_is_today.py
+++ 220125_what_day_is_today.py
@@ -3,16 +3,19 @@
 
 def what_day(year, month, day, yuandan):
     pass
+    # print(today, end=" ")
 
-if __name__ == "__main__":
-    path = "./in.txt"
+def open_today(path):
     f = open(path,"r")
     line_1 = f.readline()
     year, yuandan = line_1.split()[0], line_1.split()[1]
     count = int(f.readline())
     for i in range(count):
         line = f.readline()
-        month, day = line.split()[0], line[0].split()[1]
-        # what_day(year, month, day, yuandan)
-        print("year, month, day, yuandan = ", year, month, day, yuandan)
-    f.close()    
+        month, day = line.split()[0], line.split()[1]
+        what_day(year, yuandan,month, day)
+    f.close()
+
+if __name__ == "__main__":
+    path = "./in.txt"
+    open_today(path)
