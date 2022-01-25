b3cee8db61d73cb0be9786e7424fa0630dbfd66d
phyop <seek1andfind2@gmail.com>
2022-01-25 22:18:11 +0800

    def leap_year(year):

--- 220125_what_day_is_today.py
+++ 220125_what_day_is_today.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+def leap_year(year):
+    """閏年規則
+公元年分非4的倍數，為平年。
+公元年分為4的倍數但非100的倍數，为闰年。
+公元年分為100的倍數但非400的倍數，为平年。
+公元年分為400的倍數為閏年。"""
+    if ...:
+        leap = True
+    else ...:
+        leap  = False
+    return leap
+
 def what_day(year, month, day, yuandan):
-    pass
+    """；若輸入的「日」有誤輸出 -2"""
+    if month not in range(1,13): # 若輸入的「月」有誤輸出 -1
+        today =  -1
     # print(today, end=" ")
 
-def open_today(path):
+if __name__ == "__main__":
+    path = "./in.txt"
     f = open(path,"r")
     line_1 = f.readline()
     year, yuandan = line_1.split()[0], line_1.split()[1]
+    leap = leap_year(year)
     count = int(f.readline())
     for i in range(count):
         line = f.readline()
@@ -16,6 +32,3 @@ def open_today(path):
         what_day(year, yuandan,month, day)
     f.close()
 
-if __name__ == "__main__":
-    path = "./in.txt"
-    open_today(path)
