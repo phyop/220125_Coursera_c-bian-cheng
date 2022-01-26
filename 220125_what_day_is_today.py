0186f50ff6999aa09fdb81ce5cc22f14c860427d
phyop <seek1andfind2@gmail.com>
2022-01-27 06:42:30 +0800

    date_0to12(year, month)

--- 220125_what_day_is_today.py
+++ 220125_what_day_is_today.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+def read_input(path):
+    f = open(path,"r")
+    line_1 = f.readline()
+    year, whatday_yuandan = line_1.split()[0], line_1.split()[1]
+    count = int(f.readline())
+    month_ls = []
+    day_ls = []
+    for i in range(count):
+        line = f.readline()
+        month, day = line.split()[0], line.split()[1]
+        month_ls.append(month)
+        day_ls.append(day)
+    date_dict = dict(zip(month_ls, day_ls))
+    f.close()
+    return (year, date_dict, whatday_yuandan)
+
 def leap_year(year):
     """非4的倍數（1st多），平年。
     （下面都是對4的倍數值做討論）
@@ -16,10 +32,10 @@ def leap_year(year):
         leap = False
     return leap
 
-def date(year, month):
+def date_0to12(year, month):
     """# 若輸入的「月」有誤輸出 -1；若輸入的「日」有誤輸出 -2"""
-    month_ls = [i for i in range(1,13)]
-    day_ls = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month_ls = [i for i in range(0,13)] # 包含0月
+    day_ls = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 0月爲0天
     if leap_year(year):
         day_ls[1] = 29
     date_dict = dict(zip(month_ls, day_ls))
@@ -30,19 +46,49 @@ def what_day(year, month, day, whatday_yuandan, date_dict):
     假設1/1是禮拜6，而4/6是增加n周又5天(周四)：((d%7+6))%7=(5+6)%7=4,
     假設1/1是禮拜0(周日)，而4/6是增加n周又6天(周六)：((d%7+0))%7=(6+0)%7=6,
     假設1/1是禮拜whatday_yuandan，而month/day是：((d%7+whatday_yuandan))%7"""
-    
-    # print(today, end=" ")
+    d = day-1
+    for i in range(1, month):
+        d += date_dict.month[i]
+    whatday = ((d%7+whatday_yuandan))%7
+    return what_day
 
 if __name__ == "__main__":
     path = "./in.txt"
-    f = open(path,"r")
-    line_1 = f.readline()
-    year, whatday_yuandan = line_1.split()[0], line_1.split()[1]
+    year, test_date_dict, whatday_yuandan = read_input(path)
     leap = leap_year(year)
-    count = int(f.readline())
-    for i in range(count):
-        line = f.readline()
-        month, day = line.split()[0], line.split()[1]
-        what_day(year, whatday_yuandan,month, day)
-    f.close()
 
+    
+    date_0to12(year, month)
+    # print(whatday, end=" ")
+
+#############################################################333
+"""
+題目敘述
+寫一個程式計算給定日期為星期幾。輸入會先告訴程式某年的 1 月 1 號為星期幾，
+例如範例中 2012 年的 1 月 1 號為星期日。
+接著程式會收到一些日期，並要計算出給定日期為星期幾，
+例如範例中程式將會收到 11 月 13 號，並計算出該日期為星期二。
+
+輸入格式
+第一行包含一個西元年以及該年的一月一日為星期幾，如範例中 2012 0。
+注意，0 代表星期日，1 代表星期一，以此類推。
+第二行會告訴程式接下來將有 n 組日期需要計算。n 的範圍為 1 至 10。
+接下來的 n 行，每一行將會有一組需要計算的日期(月、日)，如範例中的 11 月 13 號。
+
+輸出格式
+共會輸出 n 個數字。我們用 0 代表星期日，1 代表星期一，以此類推。
+若輸入的「月」有誤請輸出 -1；若輸入的 「月」無誤但「日」有誤請輸出 -2。(數字間留一個空白)
+
+輸入範例
+2012 0
+5
+11 13
+11 14
+11 15
+13 1
+1 200
+
+輸出範例
+(請務必依照範例格式填寫，數字間留一個空白，否則系統將會判定答案錯誤)
+2 3 4 -1 -2
+"""
