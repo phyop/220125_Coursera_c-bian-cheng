c57fb2dcd53e5b9c8f963bb054173f54f707768e
phyop <seek1andfind2@gmail.com>
2022-01-28 10:28:32 +0800

    PASS!

--- 220125_what_day_is_today.py
+++ 220125_what_day_is_today.py
@@ -54,9 +54,9 @@ def what_day(year, month, day, whatday_yuandan, day_dict):
 def day_error(year, month, day):
     """# 若輸入的「月」有誤輸出 -1；若輸入的「日」有誤輸出 -2"""
     day_dict = date_0to12(year)
-    if month not in day_dict.keys():
+    if month not in day_dict.keys() or month == 0:
         return -1
-    if day > day_dict[month]:
+    if day > day_dict[month] or day <= 0:
         return -2
 
 if __name__ == "__main__":
@@ -100,4 +100,81 @@ if __name__ == "__main__":
 輸出範例
 (請務必依照範例格式填寫，數字間留一個空白，否則系統將會判定答案錯誤)
 2 3 4 -1 -2
+"""
+##################################################################3
+"""
+#include <stdio.h>
+
+int main ()
+{
+   int y, d;
+   int n;
+   int month, day;
+   int leap_year;
+   int days;
+
+   scanf ( "%d%d", &y, &d );
+   scanf ( "%d", &n );
+
+   // leap year or not
+   leap_year = y % 400 == 0 || ( y % 100 && y % 4 == 0 );
+
+   // calculate each input line
+   for ( int i = 0; i < n; i++ ) {
+      if ( i ) printf ( " " );
+
+      days = d;
+      scanf ( "%d%d", &month, &day );
+
+      if ( month < 1 || month > 12 ) {
+         printf ( "-1" );
+         continue;
+      }
+      if ( day < 1 ) {
+         printf ( "-2" );
+         continue;
+      }
+      else {
+         switch ( month ) {
+            case 2:
+               if ( ( leap_year && day > 29 ) ||
+                    ( !leap_year && day > 28 ) ) {
+                  printf ( "-2" );
+                  continue;
+               }
+               break;
+            case 4: case 6: case 9: case 11:
+               if ( day > 30 ) {
+                  printf ( "-2" );
+                  continue;
+               }
+               break;
+            default:
+               if ( day > 31 ) {
+                  printf ( "-2" );
+                  continue;
+               }
+         }
+      }
+
+      for ( int m = 1; m < month; m++ ) {
+         switch ( m ) {
+            case 2:
+               if ( leap_year ) days += 29;
+               else days += 28;
+               break;
+            case 4: case 6: case 9: case 11:
+               days += 30;
+               break;
+            default:
+               days += 31;
+         }
+      }
+
+      days += ( day - 1 );
+      printf ( "%d", days % 7 );
+   }
+
+   return 0;
+}
 """
